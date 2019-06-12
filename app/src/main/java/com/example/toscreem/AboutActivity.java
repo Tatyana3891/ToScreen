2dfde1addddf1e10364eea45ee7c6ff1db201595
Tatyana Glazkova <glazkova_t@ukr.net>
2019-06-13 02:37:10 +0300

    new

---
+++ app/src/main/java/com/example/toscreem/AboutActivity.java
@@ -0,0 +1,13 @@
+package com.example.toscreem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import static com.example.toscreem.R.id.textView_about_content;
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        setContentView(R.layout.activity_about);
+    }
+}
